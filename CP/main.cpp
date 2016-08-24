9c09f131a071573ae245e328b4fc0590445cd8ee
kliao <kevintliao@gmail.com>
2016-08-24 00:29:07 -0400

    Initial Commit

---
+++ CP/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CP
+//
+//  Created by Kevin Liao on 8/24/16.
+//  Copyright © 2016 Kevin Liao. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
